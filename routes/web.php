1c2e109fd591d66f6d2b1aaee3c96942f5ff7924
Fitri Anggraini <fitrianggraini96@gmail.com>
2021-09-29 17:45:47 +0700

    Init project

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/form_registrasi', function () {
+    return view('form_registrasi');
+});
+
+Route::get('/data_activity', function () {
+    return view('data_activity');
+});
+
+Route::get('/data_activity_detail', function () {
+    return view('data_activity_detail');
+});
+
+Route::get('/data_activity_edit', function () {
+    return view('data_activity_edit');
+});
